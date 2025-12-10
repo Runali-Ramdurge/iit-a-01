8269e077bd0cbb2fe49dae92853275364e1589a1
Runali Ramdurge <runaliramdurge2005@gmail.com>
2025-12-10 21:43:03 +0530

    rectangle perimeter area

---
+++ geometry.py
@@ -0,0 +1,6 @@
+
+def rectPeri(l,b):
+    print("rectangle perimeter: ", 2*(l+b))
+
+def rectArea(l,b):
+    print("rectangle area: ", l*b)
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import arithmetic
+import geometry as g
 
 print("Hello World!")
+
+num1 = int(input("Enter num 1: "))
+num2 = int(input("Enter num 2: "))
+arithmetic.add(num1,num2)
+arithmetic.sub(num1,num2)
+
+length = int(input("Enter length: "))
+breadth = int(input("Enter breadth: "))
+g.rectPeri(length,breadth)
+g.rectArea(length,breadth)
